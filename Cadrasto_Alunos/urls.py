38c00b5887236bfa2bb0e4465ed96e9a5443bb9e
Julia <menezesdasilvajulia42@gmail.com>
2024-09-10 16:02:53 -0300

    Foi editado

--- Cadrasto_Alunos/urls.py
+++ Cadrasto_Alunos/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cad/', include('CadStudent.urls')),
+    path('', include('CadStudent.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
